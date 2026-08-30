86a398f315c6e40baf8999ea3be785280764a72d
gbrl770 <ELDINGAB1410@GMAIL.COM>
2026-08-30 15:31:32 +0300

    Update game-data.js

--- data/game-data.js
+++ data/game-data.js
@@ -1,1 +1,39 @@
+const GAME_DATA = {
+    tribe: "Romans",
+    serverSpeed: 3,
 
+    resources: [
+        "wood",
+        "clay",
+        "iron",
+        "crop"
+    ],
+
+    resourceFields: {
+        wood: 6,
+        clay: 6,
+        iron: 6,
+        crop: 6
+    },
+
+    maxFieldLevel: 20,
+    maxBuildingLevel: 20,
+
+    storage: {
+        warehouse: "warehouse",
+        granary: "granary"
+    },
+
+    troops: [
+        "legionnaire",
+        "praetorian",
+        "imperian",
+        "equitesLegati",
+        "equitesImperatoris",
+        "equitesCaesaris",
+        "ram",
+        "fireCatapult",
+        "senator",
+        "settler"
+    ]
+};
