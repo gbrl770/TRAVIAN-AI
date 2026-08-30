cbb8e3f3c9e64c5a4914376e0e1e85af04d4291f
gbrl770 <ELDINGAB1410@GMAIL.COM>
2026-08-30 15:34:26 +0300

    Create buildings.js

---
+++ data/data/buildings.js
@@ -0,0 +1,16 @@
+const BUILDINGS = {
+    mainBuilding: "בניין מרכזי",
+    warehouse: "מחסן",
+    granary: "אסם",
+    residence: "מגורים מלכותיים",
+    palace: "ארמון",
+    heroMansion: "אחוזת גיבור",
+    barracks: "קסרקטין",
+    stable: "אורווה",
+    workshop: "סדנה",
+    academy: "אקדמיה",
+    smithy: "נפחייה",
+    market: "שוק",
+    rallyPoint: "נקודת מפגש",
+    cityWall: "חומת העיר"
+};
